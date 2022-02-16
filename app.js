d6adc58f4c3ead2e0b95bc935487134dab07a3eb
Rebecca Tobias <rebeccatobias123@gmail.com>
2022-02-16 12:43:22 -0500

    added dom elements

---
+++ app.js
@@ -0,0 +1,6 @@
+// DOM Elements
+const allCells = document.querySelectorAll('.cell:not(.row-top)');
+const topCells = document.querySelectorAll('.cell.row-top');
+const resetButton = document.querySelector('.reset');
+const statusSpan = document.querySelector('.status');
+const HowtoButton = document.querySelector('.instructions')
